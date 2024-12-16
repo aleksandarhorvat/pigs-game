e2733ae6d7566bd3b68ffb5d5750ae544245f0ab
Aleksandar Horvat <108305750+aleksandarhorvat@users.noreply.github.com>
2024-12-16 13:15:24 +0100

    Adedd options to menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,24 @@
 extends Node2D
 
 @onready var start_button = $Start_Button
+@onready var options_button = $Options_Button
+@onready var quit_button = $Quit_Button
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	start_button.label.text = "Start"
 	start_button.button.pressed.connect(_start_button_pressed)
+	options_button.label.text = "Options"
+	options_button.label.position.x = -30
+	options_button.button.pressed.connect(_options_button_pressed)
+	quit_button.label.text = "Quit"
+	quit_button.button.pressed.connect(_quit_button_pressed)
 
 func _start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/board.tscn")
+
+func _options_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/board.tscn")
+	
+func _quit_button_pressed() -> void:
+	get_tree().quit()
